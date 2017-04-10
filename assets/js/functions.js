2d3da0a4f7d3c32576348f1d7ad84735b71331cf
JoyceKimberly <joyce.beem@gmail.com>
2017-04-10 13:51:29 +0200

    Update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,7 @@ $(function() { // --------------------------------------------------------------
 
 $('time').each(function() {
   var $dit = $(this);
+  console.log($dit.attr('datetime'));
   var datum = new Date($dit.attr('datetime'));
   console.log(datum);
 });
