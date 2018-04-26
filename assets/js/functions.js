b03b00bc90bb1cd7d2c56a23f8c293c3a7c9e293
Jo Kim <joyce.beem@gmail.com>
2018-04-26 15:56:46 +0200

    Lang

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,26 @@
-var lang = "en";
+var lang = "nl";
 
 $(function () { // -----------------------------------------------------------------------
 
-  $("div[lang]").each(function (index, obj) {
+  $("div[lang]").each(function(index, obj) {
     var $dit = $(obj);
     if (!$dit.is('[lang="' + lang + '"]')) {
       $dit.hide();
     };
-    //console.log($dit);
   });
 
   $(".flag-link").click(function() {
     var $dit = $(this);
-    lang = "en";
+
+    if (lang == "en") {
+      lang = "nl";
+    } else {
+      lang = "en";
+    };
+
+    $("body").attr("lang", lang);
+    $dit.find("span").attr("class", "flag-icon flag-icon-" + lang);
+    
     console.log($dit);
   });
   $(".flag-link").trigger("click");
